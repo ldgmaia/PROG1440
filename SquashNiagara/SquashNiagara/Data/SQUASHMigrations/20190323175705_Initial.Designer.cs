7f70eb8238db4e98a4c64adaccca76e49a9d2857
Diego <ldgmaia@gmail.com>
2019-03-23 15:44:15 -0400

    some fix

--- SquashNiagara/SquashNiagara/Data/SQUASHMigrations/20190323175705_Initial.Designer.cs
+++ SquashNiagara/SquashNiagara/Data/SQUASHMigrations/20190323175705_Initial.Designer.cs
@@ -10,7 +10,7 @@ using SquashNiagara.Data;
 namespace SquashNiagara.Data.SQUASHMigrations
 {
     [DbContext(typeof(SquashNiagaraContext))]
-    [Migration("20190323155204_Initial")]
+    [Migration("20190323175705_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
---
+++ SquashNiagara/SquashNiagaraLib/PositionalStandings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquashNiagaraLib
+{
+    public class PositionalStandings
+    {
+
+    }
+}
