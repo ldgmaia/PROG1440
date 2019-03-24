aac085d3e23896c74f0e64352e23aa8ad3f512c3
Diego <ldgmaia@gmail.com>
2019-03-24 00:02:56 -0400

    added Positional Standings

--- SquashNiagara/SquashNiagara/Data/SQUASHMigrations/20190324035351_Initial.Designer.cs
+++ SquashNiagara/SquashNiagara/Data/SQUASHMigrations/20190324035351_Initial.Designer.cs
@@ -10,7 +10,7 @@ using SquashNiagara.Data;
 namespace SquashNiagara.Data.SQUASHMigrations
 {
     [DbContext(typeof(SquashNiagaraContext))]
-    [Migration("20190323175705_Initial")]
+    [Migration("20190324035351_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
